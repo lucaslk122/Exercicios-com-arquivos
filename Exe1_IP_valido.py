c394076e5dc8c32dfd8eb29c1ab6eacc83ddd681
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-11-04 11:58:36 -0300

    dd

--- Exe1_IP_valido.py
+++ Exe1_IP_valido.py
@@ -7,15 +7,14 @@ def validaIP(ipz):
     return True
 
 def EscreveArquivo(ip_valido,ip_invalido):
-    files = open(("Relação_de_ip's.txt" , "w"))
+    files = open("Relação_de_ips.txt" , "w")
     files.write("[ip_valido]\n")
     for i in range(len(ip_valido)):
         files.write(ip_valido[i])
     files.write("[ip_invalido]\n")
     for i in range(len(ip_invalido)):
         files.write(ip_invalido[i])
-    file.close()
-        
+    files.close()
 
 file = open("IP.txt", "r")
 ip = file.readlines()
@@ -28,5 +27,3 @@ for ipz in ip:
     else:
         ip_invalido.append(ipz)
 EscreveArquivo(ip_valido,ip_invalido)
-
-
